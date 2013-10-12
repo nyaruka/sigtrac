70f3851a24766dc574e242107af8eabcb6476f7c
Norbert Kwizera <norkans7@gmail.com>
2013-10-12 16:29:28 +0200

    added view for graph data

--- sigtrac/reports/urls.py
+++ sigtrac/reports/urls.py
@@ -1,8 +1,9 @@
-from .views import ReportCRUDL, SubmitHandler, Results
+from .views import ReportCRUDL, SubmitHandler, Results, Graph
 from django.conf.urls import patterns, url, include
 
 urlpatterns = ReportCRUDL().as_urlpatterns()
 
 urlpatterns += patterns('reports.views',
                         url(r'^results', Results.as_view(), name='reports.results'),
+                        url(r'^graph', Graph.as_view(), name='reports.graph'),
                         url(r'^submit', SubmitHandler.as_view(), name='reports.submit'))

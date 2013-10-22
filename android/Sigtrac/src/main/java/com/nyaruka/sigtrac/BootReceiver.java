90fb0c62c4c28d4c543b482f007e6f84328bd1cc
Norbert Kwizera <norkans7@gmail.com>
2013-10-22 10:16:34 +0200

    check that the service is not running before running it

--- android/Sigtrac/src/main/java/com/nyaruka/sigtrac/BootReceiver.java
+++ android/Sigtrac/src/main/java/com/nyaruka/sigtrac/BootReceiver.java
@@ -1,5 +1,6 @@
 package com.nyaruka.sigtrac;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +11,32 @@ public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+         checkService(context);
+    }
+
+
+    public static boolean checkService(Context context){
+        if(!isServiceRunning(context)){
+            startService(context);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    private static void startService(Context context){
         context.startService(new Intent(context, PingService.class));
+    }
+
+
+    private static boolean isServiceRunning(Context context) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (PingService.class.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }

4193c2a30aee1823f2f0cfd24d9c43b2a8aab4cb
Norbert Kwizera <norkans7@gmail.com>
2013-11-05 17:18:29 +0200

    nicer android cleint UI

---
+++ android/Sigtrac/src/main/java/com/nyaruka/sigtrac/ui/DinCondensedTextView.java
@@ -0,0 +1,21 @@
+package com.nyaruka.sigtrac.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class DinCondensedTextView extends TextView {
+
+    public DinCondensedTextView(Context context) {
+        super(context);
+    }
+
+
+    public DinCondensedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DinCondensedTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
---
+++ android/Sigtrac/src/main/java/com/nyaruka/sigtrac/ui/DinTextView.java
@@ -0,0 +1,22 @@
+package com.nyaruka.sigtrac.ui;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class DinTextView extends TextView {
+
+
+    public DinTextView(Context context) {
+        super(context);
+    }
+
+    public DinTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DinTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
